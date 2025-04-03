ef57e4dd5e86132078c884f9aa7c555c2fc5c6c0
IvanTang <yiwen.tang@smail.nju.edu.cn>
2025-04-03 11:55:47 +0800

    improve marketing strategy of both species, add mean line strategy for kelp, add position limit, add the structure of adjusting fair price (to be done)

--- src/strategy/market_making.py
+++ src/strategy/market_making.py
@@ -7,21 +7,44 @@ import json
 class Trader:
     
     def run(self, state: TradingState):
-        # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
-        print("traderData: " + state.traderData)
+        if not state.traderData or state.traderData.strip() == "":
+            trader_data = {}  # 如果为空，初始化为空字典
+        else:
+            try:
+                trader_data = json.loads(state.traderData)
+            except json.JSONDecodeError:
+                print(f"Warning: Failed to decode traderData: {state.traderData}")
+                trader_data = {}
+
+        print("traderData (Loaded):", json.dumps(trader_data))
         print("Observations: " + str(state.observations))
         result = {}
         print("Current position :", state.position)
-        profit_pct_limit = 0.0005
+        profit_pct_limit = 0.001
+        
+        position_limit = 50
         for product in state.order_depths:
-            print(f'Trading {product}')
-            if product == 'RAINFOREST_RESIN':
-                result[product] = self.trade_rainforest_resin(state, product, profit_pct_limit)
-                continue
             if product == 'KELP':
-                #result[product] = self.trade_kelp(state, product, profit_pct_limit)
-                continue
-        traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
+                profit_pct_limit = 0.0015
+            if product == 'RAINFOREST_RESIN':
+                profit_pct_limit = 0.0003
+            #取出历史 fair_price
+            historical_prices = trader_data.get(product, [])
+            print(f'Trading {product}')
+
+            fair_price = self.estimate_fair_price(state, product)
+            historical_prices.append(fair_price)
+
+            #控制历史数据长度
+            trader_data[product] = historical_prices[-100:]
+
+            #交易
+            result[product] = self.trade(state, product, profit_pct_limit, position_limit, historical_prices)
+
+        #存回 JSON
+        traderData = json.dumps(trader_data)
+        
+        # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
         
         conversions = 1
         return result, conversions, traderData
@@ -46,8 +69,8 @@ class Trader:
                 print('Using default price of 2025')
                 return 2025
             if product == 'RAINFOREST_RESIN':
-                print('Using default price of 0')
-                return 0
+                print('Using default price of 10000')
+                return 10000
 
 
     def get_best_price(self, orders: list, depth: int):
@@ -57,12 +80,7 @@ class Trader:
             return None, None
         return orders[depth][0], orders[depth][1] 
     
-    
-    def check_position_risk(self, position:dict, product:str) -> bool:
-        position_limit = 1000 #持仓限制
-        if product in position and abs(position[product])  >=  position_limit:
-            return False
-        return True
+
 
     
     def calculate_market_volatility(self, state: TradingState, product: str) -> float:
@@ -80,7 +98,7 @@ class Trader:
         return 0.01  # 默认最小差价
     
 
-    def calculate_ma(self, state, product:str):
+    def calculate_ma(self, state):
         try:
             trader_data = json.loads(state.traderData) if state.traderData else {}
         except json.JSONDecodeError:
@@ -106,7 +124,7 @@ class Trader:
         traderData = json.dumps(trader_data)
         return traderData
     
-    def trade_rainforest_resin(self, state, product, profit_pct_limit):
+    def trade(self, state, product, profit_pct_limit, position_limit, historical_prices):
         order_depth: OrderDepth = state.order_depths[product]
         buy_orders = [list(order) for order in order_depth.buy_orders.items()]
         sell_orders = [list(order) for order in order_depth.sell_orders.items()]
@@ -114,15 +132,31 @@ class Trader:
         position = state.position.get(product, 0)
         print("Buy Order depth : " + str(len(buy_orders)) + ", Sell order depth : " + str(len(sell_orders)))
 
-        #check order pairs from the shallowest to the deepest
+        #从浅到深检查订单簿
         i, j = 0, 0
         fair_price, expect_bid, expect_ask = self.estimate_fair_price(state, product)
+        #暂时锁死resin的fair_price
+        if product == 'RAINFOREST_RESIN':
+            fair_price = 10000
         print("Fair price : " + str(fair_price))
         print("Expect bid : " + str(expect_bid))
         print("Expect ask : " + str(expect_ask))
+        #根据持仓、动量预测、订单簿不平衡度调整fair price
+        alpha = 0
+        beta = 0
+        gamma = 0
+        print(f'Position: {position}')
+        momentum = self.price_momentum(historical_prices)
+        print(f'Price momentum: {momentum:.2f}')
+        obi = self.orderbook_imbalance(state, product)
+        print(f'Orderbook imbalance: {obi:.2f}')
+        fair_price = fair_price + (alpha * position + beta * momentum + gamma * obi)
+        print(f'Adjusted fair price: {fair_price:.2f}, Alpha {alpha:.2f}, Beta {beta:.2f}, Gamma {gamma:.2f}')
+
         while i < len(sell_orders) and j < len(buy_orders):
             ask_price, ask_amount = self.get_best_price(sell_orders, i)
             bid_price, bid_amount = self.get_best_price(buy_orders, j)
+
             if ask_price is None or bid_price is None:
                 print(f"[Warning] ask_price or bid_price is None at depth {i}, {j}")
                 break  # 防止死循环
@@ -147,8 +181,27 @@ class Trader:
 
             print(f'depth of ask order: {i}, price: {ask_price}, amount: {ask_amount}')
             print(f'depth of bid order: {j}, price: {bid_price}, amount: {bid_amount}')
+            
+            #主动交易
+            #ask_price小于fair_price，直接买入
+            if ask_price < fair_price:
+                print(f'Asking price is lower than fair price, price: {ask_price}, fair_price: {fair_price}')
+                #最大可以买入的amount
+                amount = min(-ask_amount, position_limit - position)
+                orders.append(Order(product, ask_price, amount))
+                position += amount
+                i += 1
+            #如果bid_price大于fair_price，直接买入
+            if bid_price > fair_price:
+                print(f'Bidding price is higher than fair price, price: {bid_price}, fair_price: {fair_price}')
+                #最大可卖出（做空）的amount
+                amount = min(bid_amount, position_limit + position)
+                orders.append(Order(product, bid_price, -amount))
+                position -= amount
+                j += 1
 
-
+            '''
+            这一部分也可以这样做，应该更加保守？待测试
             if ask_price < expect_ask:
                 print(f'Asking price too low, price: {ask_price}, expect_ask: {expect_ask}')
                 i += 1
@@ -156,17 +209,21 @@ class Trader:
             if bid_price > expect_bid:
                 print(f'Bidding price too high, price: {bid_price}, expect_bid: {expect_bid}')
                 j += 1
-                continue
+                continue'
+            '''
 
+            #做市
             spread = ask_price - bid_price
+            #抢单，保证成交
+            ask_price -= 1
+            bid_price += 1
             if spread / fair_price > profit_pct_limit:
                 print(f'Spread is profitable, spread_pct: {spread/fair_price*100:.2f}')
                 amount = min(-ask_amount, bid_amount)
-
                 if position > 0:  
                     # 如果当前持仓为正，优先卖出
                     print(f'Current position is positive {position}, selling')
-                    sell_amount = min(amount, position)  # 不能卖出超过持仓的数量
+                    sell_amount = min(amount, position + position_limit)
                     orders.append(Order(product, ask_price, -sell_amount))
                     sell_orders[i][1] += sell_amount
                     position -= sell_amount
@@ -174,13 +231,14 @@ class Trader:
                 elif position < 0:
                     # 如果当前持仓为负，优先买入平仓
                     print(f'Current position is negative {position}, buying to close position')
-                    buy_amount = min(amount, -position)  
+                    buy_amount = min(amount, position_limit - position)  #仓位限制
                     orders.append(Order(product, bid_price, buy_amount))
                     buy_orders[j][1] -= buy_amount
                     position += buy_amount
                     print(f'Buying {buy_amount} at {bid_price}, Current position {position}')
                 else:
                     # 如果没有持仓，同时买入和卖出
+                    amount = min(amount, position_limit) #仓位限制
                     print('No position, executing market making')
                     orders.append(Order(product, bid_price, amount))
                     orders.append(Order(product, ask_price, -amount))
@@ -196,89 +254,23 @@ class Trader:
             
         return orders
     
-
-    def trade_kelp(self, state, product, profit_pct_limit):
-        order_depth: OrderDepth = state.order_depths[product]
-        buy_orders = [list(order) for order in order_depth.buy_orders.items()]
-        sell_orders = [list(order) for order in order_depth.sell_orders.items()]
-        orders: List[Order] = []
-        position = state.position.get(product, 0)
-        print("Buy Order depth : " + str(len(buy_orders)) + ", Sell order depth : " + str(len(sell_orders)))
-
-        #check order pairs from the shallowest to the deepest
-        i, j = 0, 0
-        fair_price, expect_bid, expect_ask = self.estimate_fair_price(state, product)
-        print("Fair price : " + str(fair_price))
-        print("Expect bid : " + str(expect_bid))
-        print("Expect ask : " + str(expect_ask))
-        while i < len(sell_orders) and j < len(buy_orders):
-            ask_price, ask_amount = self.get_best_price(sell_orders, i)
-            bid_price, bid_amount = self.get_best_price(buy_orders, j)
-
-            if ask_amount == 0:
-                print(f'Ask amount at depth {i} has been fully filled. Skipping')
-                i += 1
-                continue
-            if bid_amount == 0:
-                print(f'Bid amount at depth {j} has been fully filled. Skipping')
-                j += 1
-                continue
-            
-            if ask_price is None:
-                print(f'Ask price at depth {i} is None. Skipping')
-                i += 1
-                continue
-            if bid_price is None:
-                print(f'Bid price at depth {j} is None. Skipping')
-                j += 1
-                continue
-
-            print(f'depth of ask order: {i}, price: {ask_price}, amount: {ask_amount}')
-            print(f'depth of bid order: {j}, price: {bid_price}, amount: {bid_amount}')
-
-
-            if ask_price < expect_ask:
-                print(f'Asking price too low, price: {ask_price}, expect_ask: {expect_ask}')
-                i += 1
-                continue
-            if bid_price > expect_bid:
-                print(f'Bidding price too high, price: {bid_price}, expect_bid: {expect_bid}')
-                j += 1
-                continue
-
-            spread = ask_price - bid_price
-            if spread / fair_price > profit_pct_limit:
-                print(f'Spread is profitable, spread_pct: {spread/fair_price*100:.2f}')
-                amount = min(-ask_amount, bid_amount)
-
-                if position > 0:  
-                    # 如果当前持仓为正，优先卖出
-                    print(f'Current position is positive {position}, selling')
-                    sell_amount = min(amount, position)  # 不能卖出超过持仓的数量
-                    orders.append(Order(product, ask_price, -sell_amount))
-                    sell_orders[i][1] += sell_amount
-                    position -= sell_amount
-                    print(f'Selling {sell_amount} at {ask_price}, Current position {position}')
-                elif position < 0:
-                    # 如果当前持仓为负，优先买入平仓
-                    print(f'Current position is negative {position}, buying to close position')
-                    buy_amount = min(amount, -position)  
-                    orders.append(Order(product, bid_price, buy_amount))
-                    buy_orders[j][1] -= buy_amount
-                    position += buy_amount
-                    print(f'Buying {buy_amount} at {bid_price}, Current position {position}')
-                else:
-                    # 如果没有持仓，同时买入和卖出
-                    print('No position, executing market making')
-                    orders.append(Order(product, bid_price, amount))
-                    orders.append(Order(product, ask_price, -amount))
-                    buy_orders[j][1] -= amount
-                    sell_orders[i][1] += amount
-
-                    print(f'Executing market making: Buy {amount} at {bid_price}, Sell {amount} at {ask_price}')
-                    
-                i += 1
-                j += 1
-            
-        return orders 
     
+    def orderbook_imbalance(self, state, product: str) -> float:
+        order_depth: OrderDepth = state.order_depths[product]
+        buy_orders = [(price, amount) for price, amount in order_depth.buy_orders.items()]
+        sell_orders = [(price, amount) for price, amount in order_depth.sell_orders.items()]
+
+        # 计算加权的买卖总量
+        buy_pressure = sum(price * amount for price, amount in buy_orders)
+        sell_pressure = sum(price * amount for price, amount in sell_orders)
+        
+        total_pressure = buy_pressure + sell_pressure
+
+        if total_pressure == 0:
+            return 0  # 避免除以0
+        
+        return (buy_pressure - sell_pressure) / total_pressure
+    
+    def price_momentum(self, historical_prices: List[int]) -> float:
+
+        return 0.0
